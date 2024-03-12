dde08273e5d08154b3e385150394c44473bdb598
Victor Casta <victorarroyavecastaneda@gmail.com>
2024-03-12 08:52:22 -0500

    add: pages routing
    routing by React Router Dom

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,23 +1,10 @@
+import { Routes } from '../routes/Routes'
 import '../css/App.css'
-import { Home } from './Home'
-import { MyAccount } from './MyAccount'
-import { MyOrder } from './MyOrder'
-import { MyOrders } from './MyOrders'
-import { NotFound } from './NotFound'
-import { SigIn } from './SigIn'
 
 function App() {
 
   return (
-    <>
-      <h2>lorem</h2>
-      <Home />
-      <MyAccount />
-      <MyOrder />
-      <MyOrders />
-      <NotFound />
-      <SigIn />
-    </>
+    <Routes />
   )
 }
 
---
+++ src/routes/Routes.jsx
@@ -0,0 +1,29 @@
+import { useRoutes, BrowserRouter } from 'react-router-dom'
+import { Home } from '../pages/Home'
+import { MyAccount } from '../pages/MyAccount'
+import { MyOrder } from '../pages/MyOrder'
+import { MyOrders } from '../pages/MyOrders'
+import { NotFound } from '../pages/NotFound'
+import { SigIn } from '../pages/SigIn'
+
+const AppRoutes = () => {
+  let routes = useRoutes([
+    { path: '/', element: <Home /> },
+    { path: '/my-account', element: <MyAccount /> },
+    { path: '/my-order', element: <MyOrder /> },
+    { path: '/my-orders', element: <MyOrders /> },
+    { path: '/*', element: <NotFound /> },
+    { path: '/sigin', element: <SigIn /> }
+  ]);
+  return routes
+}
+
+function Routes() {
+  return (
+    <BrowserRouter >
+      <AppRoutes />
+    </BrowserRouter>
+  )
+}
+
+export { Routes }
