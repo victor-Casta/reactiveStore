6d4dba0ffed90698a3772015e79d622c59e8c2e4
Victor Castañeda <115094771+victor-Casta@users.noreply.github.com>
2024-05-01 17:18:50 -0500

    update: best seller products estructure

--- src/components/BestProducts.jsx
+++ src/components/BestProducts.jsx
@@ -1,46 +1,37 @@
+import React, { useEffect, useState } from "react";
 import { Card } from "./Card";
 import "../css/BestProducts.css";
 
 function BestProducts() {
+  const [products, setProducts] = useState(null);
+
+  useEffect(() => {
+    fetch("https://api.escuelajs.co/api/v1/products")
+      .then((response) => response.json())
+      .then((data) => setProducts(data))
+      .catch((error) => console.error("Error fetching products:", error));
+  }, []);
+
+  const limitProductCount = products.slice(0, 4)
+
   return (
     <section className="BestProducts">
       <h2>Best Seller Products</h2>
       <div className="cards-container">
-        <Card
-          topics={["Electronics"]}
-          image="https://source.unsplash.com/random/400×400"
-          titleProduct="AirPods Pro"
-          price="$120"
-          description="Discover an exceptional listening experience with our next-generation hearing aids."
-        />
-        <Card
-          topics={["Electronics"]}
-          image="https://source.unsplash.com/random/400×400"
-          titleProduct="AirPods Pro"
-          price="$120"
-          description="Discover an exceptional listening experience with our next-generation hearing aids."
-        />
-        <Card
-          topics={["Electronics"]}
-          image="https://source.unsplash.com/random/400×400"
-          titleProduct="AirPods Pro"
-          price="$120"
-          description="Discover an exceptional listening experience with our next-generation hearing aids."
-        />
-        <Card
-          topics={["Electronics"]}
-          image="https://source.unsplash.com/random/400×400"
-          titleProduct="AirPods Pro"
-          price="$120"
-          description="Discover an exceptional listening experience with our next-generation hearing aids."
-        />
-        <Card
-          topics={["Electronics"]}
-          image="https://source.unsplash.com/random/400×400"
-          titleProduct="AirPods Pro"
-          price="$120"
-          description="Discover an exceptional listening experience with our next-generation hearing aids."
-        />
+        {limitProductCount && products.length > 0 ? (
+          limitProductCount.map((product, index) => (
+            <Card
+            topics={[product.category.name]}
+            image={product.images[0]}
+            titleProduct={product.title}
+            price={'$' + product.price}
+            description={product.description}
+            key={product.category.id}
+          />
+          ))
+        ) : (
+          <p>Loading...</p>
+        )}
       </div>
     </section>
   );
